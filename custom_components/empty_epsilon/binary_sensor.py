458af1e77651144d488904a7716679cfaf978267
Jeffrey Brite <jbrite504@gmail.com>
2026-02-11 11:00:51 -0600

    fixing binary sensor

--- custom_components/empty_epsilon/binary_sensor.py
+++ custom_components/empty_epsilon/binary_sensor.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from homeassistant.components.binary_sensor import BinarySensorEntity
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import EntityCategory
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
@@ -37,7 +38,7 @@ class EmptyEpsilonServerReachableSensor(EmptyEpsilonEntity, BinarySensorEntity):
     """Whether the EE server is reachable (HTTP responding or sACN packets)."""
 
     _attr_translation_key = "online"
-    _attr_entity_category = "diagnostic"
+    _attr_entity_category = EntityCategory.DIAGNOSTIC
 
     def __init__(self, coordinator, entry_id, config):
         super().__init__(
